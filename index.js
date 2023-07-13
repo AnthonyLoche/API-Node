2632af1c8fd3c5712d1b7cbfe7936f00790ac305
AnthonyLoche <anthonylocheifc@gmail.com>
2023-07-13 18:21:04 -0300

    Documentação da API

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const fs = require("fs");
 
 server.use(express.json({ extended: true }));
 
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger/swagger_output.json');
+
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
 const readFile = () => {
     const content = fs.readFileSync("./data/itens.json", "utf-8");
     return JSON.parse(content);
@@ -21,31 +29,35 @@ router.get("/", (req, res) => {
 })
 
 router.post("/", (req, res) => {
-    const { name, email, phone } = req.body;
+    const { name, email, phone, gender, age, profession, city } = req.body;
     const currentContent = readFile();
     const id = Math.random().toString(32).substring(2, 9);
-    currentContent.push({ id, name, email, phone });
+    currentContent.push({ id, name, email, phone, gender, age, profession, city });
     writeFile(currentContent);
-    res.send({ id, name, email, phone });
+    res.send(`Dados Cadastrados Com Sucesso`);
 })
 
 router.put("/:id", (req, res) => {
     const { id } = req.params;
-    const { name, email, phone } = req.body;
+    const { name, email, phone, gender, age, profession, city } = req.body;
     const currentContent = readFile();
     const selectedItem = currentContent.findIndex((item) => item.id === id);
-    const {id: cId, name: cName, email: cEmail, phone: cPhone } = currentContent[selectedItem];
+    const { id: cId, name: cName, email: cEmail, phone: cPhone, gender: cGender, age: cAge, profession: cProfession, city: cCity } = currentContent[selectedItem];
 
     const newObject = {
         id: cId,
         name: name ? name : cName,
         email: email ? email : cEmail,
-        phone: phone ? phone : cPhone
+        phone: phone ? phone : cPhone,
+        gender: gender ? gender : cGender,
+        age: age ? age : cAge,
+        profession: profession ? profession : cProfession,
+        city: city ? city : cCity
     };
 
     currentContent[selectedItem] = newObject;
     writeFile(currentContent);
-    res.send(currentContent);
+    res.send(`Dados Atualizados com Sucesso :) ${currentContent}`);
 })
 
 router.delete("/:id", (req, res) => {
@@ -54,11 +66,11 @@ router.delete("/:id", (req, res) => {
     const selectedItem = currentContent.findIndex((item) => item.id === id);
     currentContent.splice(selectedItem, 1);
     writeFile(currentContent)
-    res.send(true);
+    res.send("Dados Deletados com Sucesso :)");
 })
 
 server.use(router);
 server.listen(3000, () => {
-    console.log("Rodando")
+    console.log("API Funcionando com Sucesso :)")
 })
 
